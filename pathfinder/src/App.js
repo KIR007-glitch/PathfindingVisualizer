5e52cc9465b0b23601edb567761e2a987303809b
Manuja Kirinde <hher0009@student.monash.edu>
2023-01-11 02:01:46 +1100

    Changes to App.js file

--- pathfinder/src/App.js
+++ pathfinder/src/App.js
@@ -1,33 +1,15 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import './App.css';
 import PathfindingVisualizer from './PathfindingVisualizer/Node/PathfindingVisualizer';
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> main
+
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
      <PathfindingVisualizer></PathfindingVisualizer>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> main
     </div>
   );
 }
